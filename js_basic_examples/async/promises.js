ff4d0afcea59a4beded75f7f94a4312f2b71d716
Dmitry <3ddesign@bk.ru>
2021-07-09 05:54:40 +0300

    add error handling example

--- js_basic_examples/async/promises.js
+++ js_basic_examples/async/promises.js
@@ -1,29 +1,41 @@
-const myPromise = new Promise(function(resolve, reject) {
-  setTimeout(function() {
-    resolve('It worked!');
-  }, 2000);
-  // resolve('It worked - earlier!');
-});
+function setTimer(duration) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      resolve();
+    }, duration);
+    // resolve('It worked - earlier!');
+  });
+}
 
-myPromise
-  .then(function(successString) {
-    console.log(successString);
+setTimer(2000)
+  .then(function() {
+    console.log('First timer completed!');
     return 123;
   })
   .then(function(data) {
     console.log('Second then');
     console.log(data);
-    return myPromise;
+    return setTimer(3000);
   })
   .then(function(data) {
-    console.log('Third block');
+    console.log('Second timer completed!');
     console.log(data);
   });
 
-fetch('https://jsonplaceholder.typicode.com/todos/1', {})
+fetch('https://jsonplaceholder.typicode/todos/1', {})
   .then(function(response) {
+    console.log('First then');
     return response.json();
+  }, function() {
+    console.log('Handling error');
+    return 'Error handled!';
   })
-  .then(function(data) {
-    console.log(data);
-  });
+  .then(
+    function(data) {
+      console.log(data);
+    },
+    function(err) {
+      alert('This failed!');
+      console.log(err);
+    }
+  );
