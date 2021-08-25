a0e4db85f89b39f30040d89c528678e571f94538
Dmitry <3ddesign@bk.ru>
2021-08-25 09:24:24 +0300

    add left rotation

--- examples/trees/example.js
+++ examples/trees/example.js
@@ -149,8 +149,129 @@ class Tree {
   }
 }
 
-const tree = new Tree();
+class AVLTree extends Tree {
+  add(value) {
+    super.add(value);
+
+    let curNode = this.root.find(value);
+
+    while (curNode) {
+      this.balance(curNode);
+      curNode = curNode.parent;
+    }
+  }
+
+  remove(value) {
+    super.remove(value);
+
+    this.balance(this.root);
+  }
+
+  balance(node) {
+    if (node.balanceFactor < -1) {
+      if (node.right.balanceFactor < 0) {
+        this.rotateLeft(node);
+      } else if (node.right.balanceFactor > 0) {
+        this.rotateRightLeft(node);
+      }
+    } else if (node.balanceFactor > 1) {
+      if (node.left.balanceFactor < 0) {
+        this.rotateLeftRight(node);
+      } else if (node.left.balanceFactor > 0) {
+        this.rotateRight(node);
+      }
+    }
+  }
+
+  rotateLeft(node) {
+    const rightNode = node.right;
+    node.right = null;
+
+    if (node.parent) {
+      node.parent.right = rightNode;
+      node.parent.right.parent = node.parent;
+    } else if (node === this.root) {
+      this.root = rightNode;
+      this.root.parent = null;
+    }
+
+    if (rightNode.left) {
+      node.right = rightNode.left;
+      node.right.parent = node;
+    }
+
+    rightNode.left = node;
+    rightNode.left.parent = rightNode;
+  }
+
+  rotateRight(node) {
+    const leftNode = node.left;
+    node.left = null;
+
+    if (node.parent) {
+      node.parent.left = leftNode;
+      node.parent.left.parent = node.parent;
+    } else if (node === this.root) {
+      this.root = leftNode;
+      this.root.parent = null;
+    }
+
+    if (leftNode.right) {
+      node.left = leftNode.right;
+      node.left.parent = node;
+    }
+
+    leftNode.right = node;
+    leftNode.right.parent = leftNode;
+  }
+
+  rotateLeftRight(node) {
+    const leftNode = node.left;
+    node.left = null;
+
+    const leftRightNode = leftNode.right;
+    leftNode.right = null;
+
+    if (leftRightNode.left) {
+      leftNode.right = leftRightNode.left;
+      leftNode.right.parent = leftNode;
+      leftRightNode.left = null;
+    }
+
+    node.left = leftRightNode;
+    node.left.parent = node;
+
+    leftRightNode.left = leftNode;
+    leftRightNode.left.parent = leftRightNode;
+
+    this.rotateRight(node);
+  }
+
+  rotateRightLeft(node) {
+    const rightNode = node.right;
+    node.right = null;
+
+    const rightLeftNode = rightNode.left;
+    rightNode.left = null;
+
+    if (rightLeftNode.right) {
+      rightNode.left = rightLeftNode.right;
+      rightNode.left.parent = rightNode;
+      rightLeftNode.right = null;
+    }
+
+    node.right = rightLeftNode;
+    node.right.parent = node;
+
+    rightLeftNode.right = rightNode;
+    rightLeftNode.right.parent = rightLeftNode;
+
+    this.rotateLeft(node);
+  }
+}
+
+const tree = new AVLTree();
 tree.add(1);
-tree.add(2);
 tree.add(3);
+tree.add(2);
 console.log(tree);
