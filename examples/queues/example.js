2fcb06c2b808785d58726362520b750f47cb4557
Dmitry <3ddesign@bk.ru>
2021-09-01 08:47:31 +0300

    add example of processing items

--- examples/queues/example.js
+++ examples/queues/example.js
@@ -1,47 +1,67 @@
-class Node {
-  constructor(value, priority) {
-    this.value = value;
-    this.next = null;
-    this.priority = priority;
-  }
-}
-
-class PriorityQueue {
+class MaxHeap {
   constructor() {
-    this.first = null;
+    this.heapElements = [];
   }
 
-  insert(value, priority) {
-    const newNode = new Node(value, priority);
-    if (!this.first || priority > this.first.priority) {
-      newNode.next = this.first;
-      this.first = newNode;
-    } else {
-      let currentNode = this.first;
-
-      while (currentNode.next && priority < currentNode.next.priority) {
-        currentNode = currentNode.next;
-      }
-
-      newNode.next = currentNode.next;
-      currentNode.next = newNode;
+  insert(value) {
+    this.heapElements.push(value);
+    let currentElementIndex = this.heapElements.length - 1;
+    let parentElementIndex = Math.floor((currentElementIndex + 1) / 2) - 1;
+    while (
+      parentElementIndex >= 0 &&
+      this.heapElements[currentElementIndex] >
+        this.heapElements[parentElementIndex]
+    ) {
+      const parentElement = this.heapElements[parentElementIndex];
+      this.heapElements[parentElementIndex] = value;
+      this.heapElements[currentElementIndex] = parentElement;
+      currentElementIndex = parentElementIndex;
+      parentElementIndex = Math.floor((currentElementIndex + 1) / 2) - 1;
     }
   }
 
   process() {
-    const first = this.first;
-    this.first = this.first.next;
-    return first;
+    if (this.heapElements.length === 0) {
+      return null;
+    }
+    if (this.heapElements.length === 1) {
+      return this.heapElements.pop();
+    }
+    const topElement = this.heapElements[0];
+
+    this.heapElements[0] = this.heapElements.pop();
+
+    let currentElementIndex = 0;
+    let leftChildIndex = 2 * currentElementIndex + 1;
+    let rightChildIndex = 2 * currentElementIndex + 2;
+    let childElementIndex =
+      this.heapElements[rightChildIndex] &&
+      this.heapElements[rightChildIndex] >= this.heapElements[leftChildIndex]
+        ? rightChildIndex
+        : leftChildIndex;
+    while (this.heapElements[childElementIndex] && this.heapElements[currentElementIndex] <= this.heapElements[childElementIndex]) {
+      const currentNode = this.heapElements[currentElementIndex];
+      const currentChildNode = this.heapElements[childElementIndex];
+      this.heapElements[childElementIndex] = currentNode;
+      this.heapElements[currentElementIndex] = currentChildNode;
+    }
+
+    return topElement;
   }
 }
 
-const queue = new PriorityQueue();
-queue.insert('Clean up room', 1);
-queue.insert('Do taxes', 99);
-queue.insert('Learn to code', 105);
+const heap = new MaxHeap();
 
-console.log(queue);
+heap.insert(40);
+heap.insert(101);
+heap.insert(197);
+heap.insert(12);
+heap.insert(15);
+heap.insert(85);
+heap.insert(250);
 
-console.log(queue.process());
-console.log(queue.process());
-console.log(queue.process());
+heap.process();
+heap.process();
+heap.process();
+
+console.log(heap);
